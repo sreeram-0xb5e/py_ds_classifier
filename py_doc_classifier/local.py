47fefefb3b88f53f8e3385bcd360803237d2db28
sreeram-0xb5e <sreeram2910@gmail.com>
2017-04-16 13:06:42 +0530

    Initial Commit

---
+++ py_doc_classifier/local.py
@@ -0,0 +1,4 @@
+from DatumBox import DatumBox
+datum_box = DatumBox("4e9f0a6e14a83e38d9d9fac895c84e68")
+#print datum_box.twitter_sentiment_analysis("I love my cat")
+print datum_box.topic_classification("datumbox api wrapper text python code commit sign")
